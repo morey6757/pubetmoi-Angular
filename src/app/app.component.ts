622b5e465228c0b1e8dcd71ae11a7cdf8d9e5897
morey6757 <morey@live.fr>
2020-01-31 23:29:53 +0100

    environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ElementSchemaRegistry } from '@angular/compiler';
 import * as firebase from 'firebase';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,6 @@ import * as firebase from 'firebase';
 export class AppComponent {
 
   constructor() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyAl24fUzoVkB9BJecXvMXMoXoLTw6Uk1a4",
-      authDomain: "pub-et-moi-1e1bb.firebaseapp.com",
-      databaseURL: "https://pub-et-moi-1e1bb.firebaseio.com",
-      projectId: "pub-et-moi-1e1bb",
-      storageBucket: "pub-et-moi-1e1bb.appspot.com",
-      messagingSenderId: "240592766763",
-      appId: "1:240592766763:web:1e00a8fb828e64ee91e76e"
-    };
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(environment.firebaseConfig);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAl24fUzoVkB9BJecXvMXMoXoLTw6Uk1a4",
+    authDomain: "pub-et-moi-1e1bb.firebaseapp.com",
+    databaseURL: "https://pub-et-moi-1e1bb.firebaseio.com",
+    projectId: "pub-et-moi-1e1bb",
+    storageBucket: "pub-et-moi-1e1bb.appspot.com",
+    messagingSenderId: "240592766763",
+    appId: "1:240592766763:web:1e00a8fb828e64ee91e76e"
+  }
 };
